c6a8c45aa47a0a61fbb1ebcd8e0e5cacd44dd4ca
데이비드 <hadxit@gmail.com>
2021-02-28 15:16:08 +0900

    서비스워커 필요화일 갱신

--- serviceworker-files/registerServiceWorker.js
+++ serviceworker-files/registerServiceWorker.js
@@ -1,59 +1,133 @@
-/* eslint-disable no-console */
+/*=========================================================================================
+  File Name: main.js
+  Description: main vue(js) file
+  ----------------------------------------------------------------------------------------
+  Item Name: Education Template
+  Author: David
+  Author URL: https://www.notion.so/I-m-Different-I-m-David-babd4579585441d684888acd0a5ca8f8
+==========================================================================================*/
 
-import { register } from "register-service-worker";
 
-if (process.env.NODE_ENV === "production") {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    registrationOptions: {
-      scope: './'
-    },
-    ready() {
-      console.log(
-        "App is being served from cache by a service worker.\n" +
-        "For more details, visit https://goo.gl/AFskqB"
-      );
-    },
-    registered() {
-      console.log("Service worker has been registered.");
-    },
-    cached() {
-      console.log("Content has been cached for offline use.");
-    },
-    updatefound() {
-      console.log("New content is downloading.");
-    },
-    updated() {
-      console.log("New content is available; please refresh.");
-      $.confirm({
-        theme: "supervan", // 'material', 'bootstrap'
-        title: "새 버젼 릴리즈",
-        content: "업데이트를 진행하시겠습니까?",
-        buttons: {
-          confirm: {
-            text: '네',
-            btnClass: 'btn-blue',
-            keys: ['enter', 'shift'],
-            action: function() {
-              location.reload(true);
-            }
-          },
-          cancel: {
-            text: '아니오',
-            keys: ['enter', 'shift'],
-            action: function() {
-              //ignoe
-            }
-          }
-        }
-      });
-    },
-    offline() {
-      console.log(
-        "No internet connection found. App is running in offline mode."
-      );
-    },
-    error(error) {
-      console.error("Error during service worker registration:", error);
-    }
-  });
-}
+import Vue from 'vue'
+import App from './App.vue'
+
+// Vuesax Component Framework
+import Vuesax from 'vuesax'
+import 'material-icons/iconfont/material-icons.css' //Material Icons
+import 'vuesax/dist/vuesax.css' // Vuesax
+Vue.use(Vuesax)
+import "./registerServiceWorker";
+
+
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd)
+// axios
+import axios from './axios.js'
+Vue.prototype.$http = axios
+
+// API Calls
+import './http/requests'
+
+// mock
+import './fake-db/index.js'
+
+// Theme Configurations
+import '../themeConfig.js'
+
+
+// Firebase
+import '@/firebase/firebaseConfig'
+
+
+// Auth0 Plugin
+import AuthPlugin from './plugins/auth'
+Vue.use(AuthPlugin)
+
+
+// ACL
+import acl from './acl/acl'
+
+
+// Globally Registered Components
+import './globalComponents.js'
+
+
+// Styles: SCSS
+import './assets/scss/main.scss'
+
+
+// Tailwind
+import '@/assets/css/main.css'
+
+
+// Vue Router
+import router from './router'
+
+
+// Vuex Store
+import store from './store/store'
+
+
+// i18n
+import i18n from './i18n/i18n'
+
+
+// Vuexy Admin Filters
+import './filters/filters'
+
+
+// Clipboard
+import VueClipboard from 'vue-clipboard2'
+Vue.use(VueClipboard)
+
+
+// Tour
+import VueTour from 'vue-tour'
+Vue.use(VueTour)
+require('vue-tour/dist/vue-tour.css')
+
+
+// VeeValidate
+import VeeValidate from 'vee-validate'
+Vue.use(VeeValidate)
+
+
+// Google Maps
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    // Add your API key here
+    key: 'YOUR_KEY',
+    libraries: 'places' // This is required if you use the Auto complete plug-in
+  }
+})
+
+// Vuejs - Vue wrapper for hammerjs
+import { VueHammer } from 'vue2-hammer'
+Vue.use(VueHammer)
+
+
+// PrismJS
+import 'prismjs'
+import 'prismjs/themes/prism-tomorrow.css'
+
+
+// Feather font icon
+require('./assets/css/iconfont.css')
+
+
+// Vue select css
+// Note: In latest version you have to add it separately
+// import 'vue-select/dist/vue-select.css';
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  i18n,
+  acl,
+  render: h => h(App)
+}).$mount('#app')
